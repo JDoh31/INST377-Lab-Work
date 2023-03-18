08274f42bea1b40ee9c80551337cb8cf4b339e0b
Jason Doh <jdoh@terpmail.umd.edu>
2023-03-18 18:33:28 -0400

    loading animation lab 6

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -52,18 +52,23 @@ async function mainEvent() { // the async keyword means we can make API requests
   const filterButton = document.querySelector('#filter');
   const loadDataButton = document.querySelector('#data_load');
   const generateListButton = document.querySelector('#generate');
-  let currentList = []; // this is "scoped" to the main event function
+
+  const loadAnimation = document.querySelector('#load_animation');
+  loadAnimation.style.display = 'none'
+
+  let currentList = []; 
   
   /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
   loadDataButton.addEventListener('click', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
-        submitEvent.preventDefault(); 
-        console.log('form submission'); 
+        console.log('loading data'); 
+        loadAnimation.style.display = 'inline-block';
 
     // Basic GET request - this replaces the form Action
     const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
 
     // This changes the response from the GET into data we can use - an "object"
     currentList = await results.json();
+    loadAnimation.style.display = 'none'
     console.table(currentList); 
   });
 
@@ -87,9 +92,5 @@ generateListButton.addEventListener('click', (event) => {
 })
 }
 
-/*
-  This adds an event listener that fires our main event only once our page elements have loaded
-  The use of the async keyword means we can "await" events before continuing in our scripts
-  In this case, we load some data when the form has submitted
-*/
+
 document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
